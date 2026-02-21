0f9ad0b47df251b7b84a8c6bef8bb0a101e668ce
Basil Elsaid <basil_elsaid@outlook.com>
2026-02-21 21:35:32 +0100

    Setup Angular base structure + routing + material

--- frontend/app-tesi/src/app/app.routes.ts
+++ frontend/app-tesi/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'companies',
+    loadChildren: () =>
+      import('./features/companies/companies.module').then(
+        (m) => m.CompaniesModule,
+      ),
+  },
+  {
+    path: 'jobs',
+    loadChildren: () =>
+      import('./features/jobs/jobs.module').then((m) => m.JobsModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'students',
+    loadChildren: () =>
+      import('./features/students/students.module').then(
+        (m) => m.StudentsModule,
+      ),
+  },
+];
