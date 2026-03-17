40728f44b7997a76f485f9a3a649832c62a88807
Basil Elsaid <basil_elsaid@outlook.com>
2026-03-17 14:24:38 +0100

    Update database.module.ts

--- backend/app-tesi/src/database/database.module.ts
+++ backend/app-tesi/src/database/database.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/job_matching')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
